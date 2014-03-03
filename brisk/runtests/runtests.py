0f38b7d9d7fd3912e665707131f4fea953890aad
Barry Melton <barry.melton@gmail.com>
2014-03-02 19:57:40 -0500

    This ought to succeed.

--- brisk/runtests/runtests.py
+++ brisk/runtests/runtests.py
@@ -3,12 +3,14 @@
 import os
 import sys
 
+"""
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 os.environ["DJANGO_SETTINGS_MODULE"] = "brisk.runtests.settings"
 
 import django
 from django.conf import settings
 from django.test.utils import get_runner
+"""
 
 def main():
     # TestRunner = get_runner(settings)
